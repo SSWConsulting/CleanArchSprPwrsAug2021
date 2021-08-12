4c086c92bfefa2a855c823bf8fa801054a503f0b
Jason Taylor <jasontaylor@ssw.com.au>
2021-08-12 11:20:23 +1000

    GettingStarted-1+2

---
+++ CaWorkshop/CaWorkshop.WebUI/Data/ApplicationDbContextInitialiser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CaWorkshop.WebUI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaWorkshop.WebUI.Data
+{
+    public class ApplicationDbContextInitialiser
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ApplicationDbContextInitialiser(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Initialise()
+        {
+            if (_context.Database.IsSqlServer())
+            {
+                _context.Database.Migrate();
+            }
+            else
+            {
+                _context.Database.EnsureCreated();
+            }
+        }
+
+        public void Seed()
+        {
+            if (_context.TodoLists.Any())
+            {
+                return;
+            }
+
+            var list = new TodoList
+            {
+                Title = "Todo List",
+                Items =
+                {
+                    new TodoItem { Title = "Make a todo list" },
+                    new TodoItem { Title = "Check off the first item" },
+                    new TodoItem { Title = "Realise you've already done two things on the list!"},
+                    new TodoItem { Title = "Reward yourself with a nice, long nap" },
+                }
+            };
+
+            _context.TodoLists.Add(list);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ CaWorkshop/CaWorkshop.WebUI/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CaWorkshop.WebUI.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CaWorkshop.WebUI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var initialiser = services
+                        .GetRequiredService<ApplicationDbContextInitialiser>();
+
+                    initialiser.Initialise();
+                    initialiser.Seed();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services
+                        .GetRequiredService<ILogger<Program>>();
+
+                    logger.LogError(ex, "An error occurred while " +
+                        "migrating or initializing the database.");
+
+                    throw;
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}

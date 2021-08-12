983c4984eb2bdc129fc09b945f0022cec8789874
Jason Taylor <jasontaylor@ssw.com.au>
2021-08-12 16:08:26 +1000

    BeyondBasics-5

--- CaWorkshop/CaWorkshop.Application/ConfigureServices.cs
+++ CaWorkshop/CaWorkshop.Application/ConfigureServices.cs
@@ -1,5 +1,7 @@
 ﻿using System.Reflection;
 using AutoMapper;
+using CaWorkshop.Application.Common.Behaviours;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,6 +14,10 @@ namespace CaWorkshop.Application
         {
             services.AddMediatR(Assembly.GetExecutingAssembly());
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddTransient(typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
 
             return services;
         }
